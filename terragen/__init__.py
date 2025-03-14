5157be7ff1649338372d81d0b545190dcdaff222
saba <smomtselidze@altinity.com>
2025-03-14 20:41:23 +0400

    Initial commit: Add TerraGen code and documentation

---
+++ terragen/__init__.py
@@ -0,0 +1,9 @@
+"""
+TerraGen: A library for generating and manipulating Terraform configurations in HCL.
+"""
+
+__version__ = "0.1.0"
+
+from .core import HCLExpression, HCLBlock, TerraformConfig
+
+__all__ = ["HCLExpression", "HCLBlock", "TerraformConfig"]
